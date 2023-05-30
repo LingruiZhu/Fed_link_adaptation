670e05d534f26ffa62419c6b61301b01c3e80444
LingruiZhu <lingrui.zhu0521@gmail.com>
2023-05-30 16:47:58 +0200

    finshed VQ-vae method

--- auto_encoder.py
+++ auto_encoder.py
@@ -27,7 +27,7 @@ def create_autoencoder(inputs_dims:int, latent_dims:int):
     return autoencoder
     
 
-def train_autoencoder_input_compression(inputs_dims:int, latent_dims:int):
+def train_autoencoder_input_compression(inputs_dims:int, latent_dims:int, plot_figure:bool=True):
     autoencoder = create_autoencoder(inputs_dims, latent_dims)
     x_train, _, x_test, _, _ = data_preprocessing.prepare_data(num_inputs=40, num_outputs=10)
     
@@ -35,29 +35,25 @@ def train_autoencoder_input_compression(inputs_dims:int, latent_dims:int):
     x_test = np.squeeze(x_test)
     
     scaler = MinMaxScaler()
-    x_train_scaled = scaler.fit_transform(x_train)
-    x_test_scaled = scaler.fit_transform(x_test)
     
     autoencoder.fit(x_train, x_train, epochs=500, batch_size=64)
     recover_test = autoencoder.predict(x_test)
-    # recover_test_rescale = scaler.inverse_transform(recover_test)
-    # mse = mean_squared_error(x_test_scaled, recover_test)
     mse_rescale = mean_squared_error(recover_test, x_test)
-    autoencoder.save("feedback_models/autoencoder.h5")
-    # print(f"the MSE before rescale is {mse}")
+    autoencoder.save("models/ae_models/autoencoder.h5")
     print(f"the MSE after rescale is {mse_rescale}")
     
-    x_test_recover_1d = recover_test[:10,:].flatten()
-    x_test_true_1d = x_test[:10,:].flatten()
-    
-    plt.figure()
-    plt.plot(x_test_recover_1d, "r-x", label="recoverd_signal")
-    plt.plot(x_test_true_1d, "b-s", label="true signal")
-    plt.grid()
-    plt.legend()
-    plt.show()
-    plt.xlabel("time steps")
-    plt.ylabel("SINR")
+    if plot_figure:
+        x_test_recover_1d = recover_test[:10,:].flatten()
+        x_test_true_1d = x_test[:10,:].flatten()
+        
+        plt.figure()
+        plt.plot(x_test_recover_1d, "r-x", label="recoverd_signal")
+        plt.plot(x_test_true_1d, "b-s", label="true signal")
+        plt.grid()
+        plt.legend()
+        plt.show()
+        plt.xlabel("time steps")
+        plt.ylabel("SINR")
 
 
 if __name__ == "__main__":
--- vq_vae.py
+++ vq_vae.py
@@ -2,10 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Layer, Activation, Dense, Flatten, Dropout, Lambda, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, SpatialDropout2D
 from tensorflow.keras import losses
 from tensorflow.keras import backend as K
+from tensorflow.python.keras.utils import losses_utils
+
+from sklearn.metrics import mean_squared_error
+
+from Interference_prediction import data_preprocessing
 
 
 class VectorQuantizer(Layer):
@@ -19,8 +25,7 @@ class VectorQuantizer(Layer):
 
         # Initialize the embeddings which we will quantize.
         w_init = tf.random_uniform_initializer()
-        self.embeddings = tf.Variable(
-            initial_value=w_init(
+        self.embeddings = tf.Variable(            initial_value=w_init(
                 shape=(self.embedding_dim, self.num_embeddings), dtype="float32"
             ),
             trainable=True,
@@ -69,6 +74,27 @@ class VectorQuantizer(Layer):
         return encoding_indices
 
 
+# def calculate_vae_loss(encoder_output, quantized_latent_variable, variance, beta):
+#     """Define the loss function of VAE based on the equation (3) from "Neural Discrete Representation Learning".
+
+#     Args:
+#         encoder_output (tf.Tensor): The output of the encoder.
+#         quantized_latent_variable (tf.Tensor): The quantized latent variable.
+#         variance (float): The variance of the encoder output.
+#         beta (float): The weight of the commitment loss in the total loss.
+        
+#     Returns:
+#         The VQ-VAE loss function.
+#     """
+#     def vq_vae_loss(x, x_hat):
+#         reconstruction_loss = losses.mean_squared_error(x, x_hat) / variance
+#         quantizer_loss = losses.mean_squared_error(tf.stop_gradient(encoder_output), quantized_latent_variable)
+#         commitment_loss = losses.mean_squared_error(encoder_output, tf.stop_gradient((quantized_latent_variable)))
+#         loss = reconstruction_loss + quantizer_loss + beta*commitment_loss
+#         return loss
+#     return vq_vae_loss
+
+
 def create_encoder(input_dim, latent_dim):
     inputs = Input(shape=(input_dim,))
     hidden1 = Dense(units=int(input_dim/2), activation="relu")(inputs)
@@ -88,20 +114,127 @@ def create_decoder(latent_dim, output_dim):
 def create_quantized_autoencoder(input_dim, latent_dim, output_dim):
     encoder = create_encoder(input_dim, latent_dim)
     decoder = create_decoder(latent_dim, output_dim)
-    quantizer = VectorQuantizer(num_embeddings=1, embedding_dim=16)
+    quantizer = VectorQuantizer(num_embeddings=128, embedding_dim=latent_dim)
     
     inputs = Input(shape=(input_dim,))
     encoder_outputs = encoder(inputs)
     encoder_outputs_quantized = quantizer(encoder_outputs)
+    
     decoder_output = decoder(encoder_outputs_quantized)
+    # decoder_output = decoder(encoder_outputs)       # here to make a shortcut to skip quant layer
+    
     vector_quant_autoencoder = Model(inputs=inputs, outputs=decoder_output, name="vector_quantized_autoencoder")
+    # vector_quant_autoencoder.compile(loss=vae_loss, optimizer="adam")
     return vector_quant_autoencoder
 
 
+class VQVAETrainer(Model):
+    def __init__(self, train_variance, input_dim, latent_dim=10, num_embeddings=1, **kwargs):
+        super().__init__(**kwargs)
+        self.train_variance = train_variance
+        self.latent_dim = latent_dim
+        self.input_dim = input_dim
+        self.num_embeddings = num_embeddings
+
+        self.vqvae = create_quantized_autoencoder(self.input_dim, self.latent_dim, self.input_dim)
+        self.vqvae.summary()
+
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.vq_loss_tracker = keras.metrics.Mean(name="vq_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.vq_loss_tracker,
+        ]
+
+    def train_step(self, x):
+        with tf.GradientTape() as tape:
+            # Outputs from the VQ-VAE.
+            reconstructions = self.vqvae(x)
+
+            # Calculate the losses.
+            reconstruction_loss = (
+                tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
+            )
+            total_loss = reconstruction_loss + sum(self.vqvae.losses)
+
+        # Backpropagation.
+        grads = tape.gradient(total_loss, self.vqvae.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.vqvae.trainable_variables))
+
+        # Loss tracking.
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.vq_loss_tracker.update_state(sum(self.vqvae.losses))
+
+        # Log results.
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "vqvae_loss": self.vq_loss_tracker.result(),
+        }
+    
+    
+    def predict(self, x):
+        return self.vqvae.predict(x)
+
+
+def train_vq_vae(inputs_dims:int, latent_dims:int, plot_figure:bool=True):
+    x_train, _, x_test, _, _ = data_preprocessing.prepare_data(num_inputs=40, num_outputs=10)
+    x_train = np.squeeze(x_train)
+    x_test = np.squeeze(x_test)
+    
+    x_train = np.array(x_train)
+    x_test = np.array(x_test)
+    
+    variance = np.var(x_train)
+    
+    vq_vae_trainer = VQVAETrainer(variance, inputs_dims, latent_dims)
+    vq_vae_trainer.compile(optimizer="adam")
+    
+    x_train_hat = vq_vae_trainer.predict(x_train)
+    
+    vq_vae_trainer.fit(x=x_train, epochs=500, batch_size=64)
+    vq_vae_trainer.save_weights("models/vq_vae_models/vq_vae.h5")
+    x_test_pred = vq_vae_trainer.predict(x_test)
+    mse = mean_squared_error(x_test, x_test_pred)
+    
+    if plot_figure:
+        x_test_recover_1d = x_test_pred[:10,:].flatten()
+        x_test_true_1d = x_test[:10,:].flatten()
+        
+        plt.figure()
+        plt.plot(x_test_recover_1d, "r-x", label="recoverd_signal")
+        plt.plot(x_test_true_1d, "b-s", label="true signal")
+        plt.grid()
+        plt.legend()
+        plt.show()
+        plt.xlabel("time steps")
+        plt.ylabel("SINR")
+    return mse
+    
+
 if __name__ == "__main__":
-    input_dim = 40
-    latent_dim = 10
-    vq_ae = create_quantized_autoencoder(input_dim=input_dim, latent_dim=latent_dim, output_dim=input_dim)
-    print(vq_ae.losses)
-    vq_ae.summary()
+    # input_dim = 40
+    # latent_dim = 10
+    # vq_ae = create_quantized_autoencoder(input_dim=input_dim, latent_dim=latent_dim, output_dim=input_dim)
+    # print(vq_ae.losses)
+    # vq_ae.summary()
+    
+    train_vq_vae(inputs_dims=40, latent_dims=10, plot_figure=True)
+    # x_train, _, x_test, _, _ = data_preprocessing.prepare_data(num_inputs=40, num_outputs=10)
+    # encoder = create_encoder(input_dim=40, latent_dim=10)
+    # quant_layer = VectorQuantizer(num_embeddings=128, embedding_dim=10)
+    # decoder = create_decoder(latent_dim=10, output_dim=40)
+    
+    # encoder_output = encoder(x_train)
+    # encoder_output_quantized = quant_layer(encoder_output)
+    # decoder_output = decoder(encoder_output_quantized)
+
     
